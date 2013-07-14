19eab9cc92f8fa47777c145d803458bae1071f6f
Jarrad Battaglia <jmb459@cs.drexel.edu>
2013-07-14 18:20:05 -0400

    Build files (default)

---
+++ linux-2.6.22.19-cs543/include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 1 */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 Sun Jul 14 18:14:41 EDT 2013"
+#define LINUX_COMPILE_TIME "18:14:41"
+#define LINUX_COMPILE_BY "jmb459"
+#define LINUX_COMPILE_HOST "tux64-14.cs.drexel.edu"
+#define LINUX_COMPILE_DOMAIN "cs.drexel.edu"
+#define LINUX_COMPILER "gcc version 4.6.3 (Ubuntu/Linaro 4.6.3-1ubuntu5) "
